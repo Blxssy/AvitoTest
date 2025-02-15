c5cc0e39540624134106f57b52b146505fe651f6
blxsyy <roma.asav@gmail.com>
2025-02-15 18:31:42 +0300

    e2e tests

--- internal/transport/http/v1/coin_test.go
+++ internal/transport/http/v1/coin_test.go
@@ -1,51 +1,99 @@
 package v1_test
 
 import (
-	"bytes"
-	"encoding/json"
+	"github.com/Blxssy/AvitoTest/internal/services"
+	"github.com/Blxssy/AvitoTest/internal/services/mocks"
 	"github.com/Blxssy/AvitoTest/internal/transport/http/v1"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func setupApp() *fiber.App {
+func TestAuthHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockCoinService(ctrl)
+
 	app := fiber.New()
-	handler := v1.NewHandler(v1.HandlerConfig{})
+	handler := v1.NewHandler(v1.HandlerConfig{
+		CoinService: mockService,
+		Logger:      nil,
+	})
 	handler.Init(app)
-	return app
-}
+	app.Post("/api/auth", handler.Auth)
 
-func TestBuyItem(t *testing.T) {
-	app := setupApp()
+	requestBody := `{"username":"test","password":"123"}`
+	mockService.EXPECT().Auth(gomock.Any(), services.AuthParams{
+		Username: "test",
+		Password: "123",
+	}).Return("valid_token", nil)
 
-	// Получение токена
-	token := "your_test_token"
-
-	// Покупка мерча
-	req := httptest.NewRequest(http.MethodGet, "/api/buy/item1", nil)
-	req.Header.Set("Authorization", "Bearer "+token)
+	req := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/auth", strings.NewReader(requestBody))
+	req.Header.Set("Content-Type", "application/json")
 	resp, _ := app.Test(req)
+
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
 
-func TestSendCoin(t *testing.T) {
-	app := setupApp()
+func TestBuyItemHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	// Получение токена
-	token := "your_test_token"
+	mockService := mocks.NewMockCoinService(ctrl)
 
-	// Передача монеток
-	sendCoinReq := v1.SendCoinRequest{
-		ReceiverUsername: "receiveruser",
-		Amount:           10,
-	}
-	sendCoinReqBody, _ := json.Marshal(sendCoinReq)
-	req := httptest.NewRequest(http.MethodPost, "/api/sendCoin", bytes.NewReader(sendCoinReqBody))
+	app := fiber.New()
+	handler := v1.NewHandler(v1.HandlerConfig{
+		CoinService: mockService,
+		Logger:      nil,
+	})
+	handler.Init(app)
+	app.Get("/api/buy/:item", handler.BuyItem)
+
+	mockService.EXPECT().BuyItem(gomock.Any(), services.BuyItemParams{
+		Token: "valid_token",
+		Item:  "powerbank",
+	}).Return(nil)
+
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:8080/api/buy/powerbank", nil) // Используем реальное значение
+	req.Header.Set("Authorization", "Bearer valid_token")
+
+	resp, err := app.Test(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestSendCoinsHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockCoinService(ctrl)
+
+	app := fiber.New()
+	handler := v1.NewHandler(v1.HandlerConfig{
+		CoinService: mockService,
+		Logger:      nil,
+	})
+	handler.Init(app)
+	app.Post("/api/sendCoin", handler.Transaction)
+
+	requestBody := `{"toUser": "Bill", "amount": 100}`
+	mockService.EXPECT().SendCoins(gomock.Any(), services.TransactionParams{
+		Token:            "valid_token",
+		ReceiverUsername: "Bill",
+		Amount:           100,
+	}).Return(nil)
+
+	req := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/sendCoin", strings.NewReader(requestBody))
+	req.Header.Set("Authorization", "Bearer valid_token")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+token)
 	resp, _ := app.Test(req)
+
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
